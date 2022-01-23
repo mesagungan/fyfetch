4132ba8fe7997d30f92521edc668db46a976bb63
mesagungan <mi03to08@gmail.com>
2022-01-23 01:38:59 +0100

    works

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 //_ "github.com/joho/godotenv/autoload"
 
-// This example demonstrates how to authenticate with Spotify using the authorization code flow with PKCE.
-// In order to run this example yourself, you'll need to:
-//
-//  1. Register an application at: https://developer.spotify.com/my-applications/
-//       - Use "http://localhost:8080/callback" as the redirect URI
-//  2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
 package main
 
 import (
@@ -17,8 +11,6 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 
-	"golang.org/x/oauth2"
-
 	"github.com/zmb3/spotify/v2"
 )
 
@@ -28,14 +20,9 @@ import (
 const redirectURI = "http://localhost:8080/callback"
 
 var (
-	auth  = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate, spotifyauth.ScopeUserModifyPlaybackState))
+	auth  = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate, spotifyauth.ScopeUserReadCurrentlyPlaying, spotifyauth.ScopeUserTopRead))
 	ch    = make(chan *spotify.Client)
 	state = "abc123"
-	// These should be randomly generated for each request
-	//  More information on generating these can be found here,
-	// https://www.oauth.com/playground/authorization-code-with-pkce.html
-	codeVerifier  = "w0HfYrKnG8AihqYHA9_XUPTIcqEXQvCQfOF2IitRgmlF43YWJ8dy2b49ZUwVUOR.YnvzVoTBL57BwIhM4ouSa~tdf0eE_OmiMC_ESCcVOe7maSLIk9IOdBhRstAxjCl7"
-	codeChallenge = "ZhZJzPQXYBMjH8FlGAdYK5AndohLzFfZT-8J7biT7ig"
 )
 
 func main() {
@@ -44,31 +31,50 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Got request for:", r.URL.String())
 	})
-	go http.ListenAndServe(":8080", nil)
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-	url := auth.AuthURL(state,
-		oauth2.SetAuthURLParam("code_challenge_method", "S256"),
-		oauth2.SetAuthURLParam("code_challenge", codeChallenge),
-	)
+	url := auth.AuthURL(state)
 	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
 
 	// wait for auth to complete
 	client := <-ch
 
+	ctx := context.Background()
 	// use the client to make calls that require authorization
-	user, err := client.CurrentUser(context.Background())
+	user, err := client.CurrentUser(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("You are logged in as:", user.ID)
-	lol := client.Pause(context.Background())
-	fmt.Println(lol)
-	client.Play(context.Background())
+	playing, err := client.PlayerCurrentlyPlaying(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	topArtist, err := client.CurrentUsersTopArtists(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	topTracks, err := client.CurrentUsersTopTracks(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("username: ", user.DisplayName)
+
+	fmt.Println("track artist: ", playing.Item.Artists[0].Name)
+	fmt.Println("track album: ", playing.Item.Album.Name)
+	fmt.Println("track name: ", playing.Item.Name)
+
+	fmt.Println("top artist: ", topArtist.Artists[0].Name)
+	fmt.Println("top track: ", topTracks.Tracks[0].Name)
+
 }
 
 func completeAuth(w http.ResponseWriter, r *http.Request) {
-	tok, err := auth.Token(r.Context(), state, r,
-		oauth2.SetAuthURLParam("code_verifier", codeVerifier))
+	tok, err := auth.Token(r.Context(), state, r)
 	if err != nil {
 		http.Error(w, "Couldn't get token", http.StatusForbidden)
 		log.Fatal(err)
@@ -77,9 +83,9 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		log.Fatalf("State mismatch: %s != %s\n", st, state)
 	}
+
 	// use the token to get an authenticated client
 	client := spotify.New(auth.Client(r.Context(), tok))
 	fmt.Fprintf(w, "Login Completed!")
 	ch <- client
-
 }
